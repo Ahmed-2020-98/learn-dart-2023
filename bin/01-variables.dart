f1a36b53636483beaa9c5382499d278f3fca122e
Ahmed-2020-98 <57391305+Ahmed-2020-98@users.noreply.github.com>
2023-02-15 19:27:38 +0200

    variables

---
+++ bin/01-variables.dart
@@ -0,0 +1,10 @@
+void main(List<String> arguments) {
+  int number = 4;
+  double number2 = 2.5;
+  String name = "ahmed";
+  bool isSuccess = false;
+  List subjects = ['math', 'english', 33];
+  Map n = {"name": "ahmed", "age": 24, "isSuccess": true};
+
+  print(n.values);
+}
--- bin/learndart.dart
+++
@@ -1,5 +0,0 @@
-import 'package:learndart/learndart.dart' as learndart;
-
-void main(List<String> arguments) {
-  print('Hello world!');
-}
